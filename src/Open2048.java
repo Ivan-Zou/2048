ec53e712676f629a0c4a6088c2734a6db8229e10
Ivan Zou <ivanzou2@gmail.com>
2022-05-14 09:11:16 -0400

    added import

--- src/Open2048.java
+++ src/Open2048.java
@@ -1,8 +1,10 @@
+import game.Run2048;
+
 import javax.swing.SwingUtilities;
 
 public class Open2048 {
     public static void main(String[] args) {
-        Runnable game = new game.Run2048();
+        Runnable game = new Run2048();
         SwingUtilities.invokeLater(game);
     }
 }
