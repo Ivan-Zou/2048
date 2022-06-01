7d7f20adef07b6f120dddc3a77111d5587d27c0d
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 11:27:50 -0400

    rename variables

--- src/game/Run2048.java
+++ src/game/Run2048.java
@@ -12,38 +12,37 @@ import java.awt.BorderLayout;
 public class Run2048 implements Runnable {
     public void run() {
         final JFrame frame = new JFrame("2048");
-        frame.setLocation(Constants.WINDOW_LOCATION_X, Constants.WINDOW_LOCATION_Y);
 
-        final JPanel status_panel = new JPanel();
-        frame.add(status_panel, BorderLayout.NORTH);
+        final JPanel statusPanel = new JPanel();
+        frame.add(statusPanel, BorderLayout.NORTH);
 
         // Points labels
         final JLabel pointLabel = new JLabel("Points: " + 0);
         pointLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        status_panel.add(pointLabel);
+        statusPanel.add(pointLabel);
 
         Scores scores = new Scores();
         final JLabel highScoreLabel = new JLabel("HighScore: " + scores.getHighScore());
         highScoreLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        status_panel.add(highScoreLabel);
+        statusPanel.add(highScoreLabel);
 
         // Status label
         final JLabel statusLabel = new JLabel("Running...");
         statusLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-        status_panel.add(statusLabel);
+        statusPanel.add(statusLabel);
 
         // Creates the Game
         final GameMechanics field = new GameMechanics(statusLabel, pointLabel, highScoreLabel);
         frame.add(field, BorderLayout.CENTER);
 
-        final JPanel control_panel = new JPanel();
-        frame.add(control_panel, BorderLayout.SOUTH);
+        final JPanel controlPanel = new JPanel();
+        frame.add(controlPanel, BorderLayout.SOUTH);
 
         // Reset button
         final JButton reset = new JButton("New Game");
         reset.setFocusable(false);
         reset.addActionListener(actionEvent -> field.reset());
-        control_panel.add(reset);
+        controlPanel.add(reset);
 
         // Save Game button
         final JButton save = new JButton("Save Game");
@@ -55,17 +54,18 @@ public class Run2048 implements Runnable {
                 field.save();
             }
         });
-        control_panel.add(save);
+        controlPanel.add(save);
 
         // Load Game button
         final JButton load = new JButton("Load Game");
         load.setFocusable(false);
         load.addActionListener(actionEvent -> field.load());
-        control_panel.add(load);
+        controlPanel.add(load);
 
         frame.pack();
+        frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setLocation(Constants.WINDOW_LOCATION_X, Constants.WINDOW_LOCATION_Y);
     }
 }
