40a847129d5703bb9d11024690e330e02519ca59
Ivan Zou <ivanzou2@gmail.com>
2022-06-01 11:34:22 -0400

    changed label, HighScore to High Score

--- src/game/Run2048.java
+++ src/game/Run2048.java
@@ -22,7 +22,7 @@ public class Run2048 implements Runnable {
         statusPanel.add(pointLabel);
 
         Scores scores = new Scores();
-        final JLabel highScoreLabel = new JLabel("HighScore: " + scores.getHighScore());
+        final JLabel highScoreLabel = new JLabel("High Score: " + scores.getHighScore());
         highScoreLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
         statusPanel.add(highScoreLabel);
 
